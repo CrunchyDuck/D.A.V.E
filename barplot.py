3417598e1ab6ae569f5b2bc36199b3f2041795bd
CrunchyDuck <43281440+CrunchyDuck@users.noreply.github.com>
2021-09-08 17:18:08 +0100

    First working version

--- barplot.py
+++ barplot.py
@@ -1,139 +1,82 @@
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-import sys
-import json
 import math
-from collections import Counter
-
-# About how we store data:
-# It would use an unreasonable amount of space to track all message counts from all members for all dates.
-# Over 1 year with 100,000 members, that'd be 36,500,000 entries! And we'd need to run lots of calculations on each entry!
-# Instead, if we want the top 10 members, we'll keep only keep track of the top 15 at any point.
-# That doesn't mean members that aren't in the top 15 are ignored - If they break into top 15 they'll start getting saved.
-# This just saves a lot of storage space on data we wouldn't be using otherwise.
-#
-# The reason we save the top 15 instead of only the top 10 is to allow people to cleanly animate dropping in/out of the top rankings.
-# Otherwise if someone leaves top 10, they'd just suddenly disappear and be replaced, rather than animate out.
-#
-# The data file itself is quite simple, based off of CSV files.
-# The first line gives information about how the data was stored, e.g how many users were tracked at once.
-# After that, each line represents 1 frame.
-# Each line will have a date as the first value.
-# After this, there will be 2 values for each member that was stored, their name and their message count.
-# E.G if CrunchyDuck has 5 messages, and Dave has 10, the line would be:
-# 2021-09-06,Dave,10,CrunchyDuck,5
-# You can see that each value is separated by a comma. Each row should have the same number of entries.
-
-
-class Keyframe:
-    def __init__(self, name, count):
-        self.name = name  # Username
-        self.count = count  # Number of messages
-        self.color = None  # Colour of their bar. This is assigned by ColorTracker.get_color()
-        self.y_position = None  # Used by AnimationFrame
-
-    # These functions allow comparisons to work between Keyframes, E.G dp1 < dp2.
-    def __repr__(self):
-        return f"[{self.name}, {self.count}, {self.color}, {self.y_position}]"
-
-    def __lt__(self, other):
-        return self.count < other.count
-
-    def __gt__(self, other):
-        return self.count > other.count
-
-    def __eq__(self, other):
-        return self.count == other.count
-
-    def __le__(self, other):
-        return self.count <= other.count
-
-    def __ge__(self, other):
-        return self.count >= other.count
-
-    def __ne__(self, other):
-        return self.count != other.count
-
-
-class ColorTracker:
-    """This object handles the assignment of colours to Keyframe objects.
-    This is necessary to keep the colour of Keyframes constant while they're being tracked.
-    When a Keyframe leaves the tracked list of members, its colour is taken from it and given to something else.
-    """
-    def __init__(self, colors):
-        # These are all of the colours that haven't been claimed yet.
-        self.unreserved_colors = colors
-
-        # These are all of the colours reserved by Keyframes.
-        # If any of these are unclaimed in last_frame at the end of the parsing, they're marked as unreserved - aka the Keyframe dropped out.
-        # Stored as {Keyframe.name: color}
-        self.reserved_colors_last_frame = {}
-        self.reserved_colors_this_frame = {}
-
-        # A list of Keyframes that have requested a colour, and are waiting.
-        # When a colour is available, which will always inevitably happen, it's given to someone on this list.
-        self.data_points_awaiting_color = []
-
-    def get_color(self, dp: Keyframe):
-        """Gets the colour this data point had last frame. If not found, puts it in the waiting line for colours."""
-        if dp.name in self.reserved_colors_last_frame:
-            col = self.reserved_colors_last_frame.pop(dp.name)
-            dp.color = col
-            self.reserved_colors_this_frame[dp.name] = col
-        else:
-            self.data_points_awaiting_color.append(dp)
-
-    def finish_frame(self):
-        """Should be called at the end of each frame.
-        This fills reserved_colors_last_frame and assigns all data_points_awaiting_color
-        """
-        # Put any remaining colours into the unclaimed colours bag.
-        self.unreserved_colors += self.reserved_colors_last_frame.values()
-
-        for dp in self.data_points_awaiting_color:  # New arrivals to the ranked peeps
-            col = self.unreserved_colors.pop(0)  # Get colour
-            self.reserved_colors_this_frame[dp.name] = col  # Reserve colour
-            dp.color = col  # Apply colour
-
-        self.data_points_awaiting_color = []
-        self.reserved_colors_last_frame = self.reserved_colors_this_frame
-        self.reserved_colors_this_frame = {}
-
-
-class AnimationFrame:
-    """A frame contains all keyframes in a frame."""
-
-    def __init__(self, title=None):
-        self.title = title  # Date to display on the timelapse.
-        self.keyframes = {}
+import pandas
 
 
 class MatAnimation:
-    def __init__(self, ax, bars_to_display, frames):
+    def __init__(self, ax, bars_to_display, users, titles):
         self.ax = ax
         self.bars_to_display = bars_to_display
-        self.frames = frames
+        self.users = users
+        self.titles = titles
 
     def animate_frame(self, frame_num):
         self.ax.clear()
         style_graph(self.ax)
-        frame = self.frames[frame_num]
+        #frame = self.frames[frame_num]
         positions = []
         bar_sizes = []
         cols = []
         labels = []
-        for k, keyframe in frame.keyframes.items():
-            positions.append(keyframe.y_position)
-            bar_sizes.append(keyframe.count)
-            cols.append(keyframe.color)
-            labels.append(keyframe.name)
-            if self.bars_to_display > keyframe.y_position > 0:  # Python 3.9 added this, and PyCharm wants me to use it.
-                self.ax.text(keyframe.count + 3, keyframe.y_position, round(keyframe.count), verticalalignment="center")
+        self.ax.text(0.95, 0.15, self.titles[frame_num], transform=self.ax.transAxes, horizontalalignment="right", size=22)
+        for user in self.users:
+            y_pos = user.y_pos[frame_num]
+            count = user.count[frame_num]
+            if count == 0:
+                continue
+
+            cols.append(user.color)
+            labels.append(user.name)
+            bar_sizes.append(count)
+            positions.append(y_pos)
+            #if self.bars_to_display > y_pos > 0:  # Python 3.9 added this, and PyCharm wants me to use it.
+            self.ax.text(count + 3, y_pos, round(count), verticalalignment="center")
 
         self.ax.barh(y=positions, width=bar_sizes, color=cols, tick_label=labels)
-        self.ax.set_ylim([self.bars_to_display - 0.5, -0.5])  # Range of bars to show. This is offset by -0.5 so we don't cut the bars in half.
-        self.ax.set_title("test")
+        #print(self.ax.get_ylim())
+        #print(len(self.users))
+        top = len(self.users)+0.5
+        bottom = top - self.bars_to_display
+        self.ax.set_ylim(bottom, top)  # Range of bars to show. This is offset by -0.5 so we don't cut the bars in half.
+        #self.ax.set_title(self.titles[frame_num])
         return [self.ax]
+
+
+class User:
+    """Each user tracks their own animation data."""
+    interp_per_update = 1/10  # How many frames it takes for the animation to complete.
+
+    def __init__(self, name=None, color=None, default_pos=11):
+        self.name = name  # Username
+        self.color = color  # Colour of their bar. This is assigned by ColorTracker.get_color()
+
+        self.count = []  # Number of messages
+        self.y_pos = []  # Used by AnimationFrame
+
+        # Used to interpolate towards new y positions.
+        self.interp_pos = 0  # How far
+        self.last_pos = None
+        self.target_pos = default_pos
+
+    def update_y_pos(self, position):
+        if not self.y_pos:  # Initialization
+            self.y_pos.append(position)
+            self.last_pos = position
+            self.target_pos = position
+
+        if position == self.target_pos:  # Target hasn't changed.
+            if self.interp_pos >= 1:  # Target reached
+                self.y_pos.append(self.target_pos)
+            else:
+                self.interp_pos += self.interp_per_update
+                self.y_pos.append(slerp(self.last_pos, self.target_pos, self.interp_pos))
+        else:  # New target acquired.
+            self.last_pos = self.y_pos[-1]
+            self.target_pos = position
+            #self.interp_pos = abs((self.y_pos[-1] - self.last_pos) / (self.target_pos - self.last_pos))
+            self.interp_pos = 0
+            self.update_y_pos(position)
 
 
 def lerp(a, b, t):
@@ -146,26 +89,6 @@ def slerp(a, b, t):
     return lerp(a, b, new_t)
 
 
-def parse_row_data(data: str):
-    """Makes the values to use in the bar chart
-
-    Returns:
-        [date, list:Keyframes]
-    """
-    data = json.loads(data)
-
-    date = data[0]
-    users = data[1]
-    data_points = []
-    for user in users:
-        name = user["ident"]
-        count = user["count"]
-        dp = Keyframe(name, count)
-        data_points.append(dp)
-
-    return date, data_points
-
-
 def style_graph(ax):
     """Formats the graph to look a little prettier."""
     ax.set_facecolor('.8')
@@ -175,87 +98,71 @@ def style_graph(ax):
     [spine.set_visible(False) for spine in ax.spines.values()]
 
 
-def main():
-    data_file_path = "keyframes.txt"
-    data_file = open(data_file_path, "r")
+def prepare_dataframe(dataframe, expand_factor):
+    # Expand out the dataframe
+    dataframe = dataframe.reset_index()  # Not certain what this does but it's vital.
+    dataframe.index = dataframe.index * expand_factor  # Change indexes from 0,1,2... to 0,30,60...
+    last_index = dataframe.index[-1] + 1
+    dataframe = dataframe.reindex(range(last_index))  # Inset empty rows between new indexes.
+    dataframe["date"] = dataframe["date"].fillna(method="ffill")  # Fill in column
 
-    # == Constants == #
-    file_metadata = data_file.readline().split(",")
-    dp_display_number = int(file_metadata[0])  # How many Keyframes should be shown at once.
-    dp_tracked_number = int(file_metadata[1])  # How many Keyframes in total are tracked. The extra values are used for animations.
-    frames_per_keyed_frame = 60  # Distance between each keyed frame. Always at least 1.
+    # Interpolate values
+    dataframe = dataframe.interpolate()
+    #dataframe = dataframe.round()  # We can't have a fraction of a message.
 
-    # == Loop initialization == #
-    # Colours we'll be using.
-    col_count = dp_tracked_number + 1  # We add one to prevent wrapping, where colour 0 is the same as colour -1
-    cols = plt.get_cmap("twilight_shifted", col_count)  # See https://matplotlib.org/stable/gallery/color/colormap_reference.html
-    cols = cols(range(col_count)).tolist()  # Convert the colours into scalar RGBA values.
-    color_tracker = ColorTracker(cols)  # Object that handles bar colour assignment.
-    keyed_frames = []
-
-    data_line = data_file.readline()  # First line of data
-    # ==== Create frame data ==== #
-    while data_line:
-        # Creates keyframes with a name and message count.
-        date, keyframes = parse_row_data(data_line)
-
-        # Get the colours for each bar.
-        for keyframe in keyframes:
-            color_tracker.get_color(keyframe)
-        color_tracker.finish_frame()
-
-        # Calculate ranking of data
-        keyframes = sorted(keyframes, reverse=True)
-        for i in range(len(keyframes)):
-            keyframes[i].y_position = i
-
-        # Save all keyframes to a frame.
-        frame = AnimationFrame(date)  # Contains all keyframes, indexed by a unique identifier.
-        for keyframe in keyframes:
-            frame.keyframes[keyframe.name] = keyframe  # Each keyframe needs a unique identifier; In this case, the username.
-
-        keyed_frames.append(frame)  # Add this frame to the list of frames
-        data_line = data_file.readline()
-
-    # ==== Fill in frame data ==== #
-    frames = [AnimationFrame() for i in range(len(keyed_frames) * frames_per_keyed_frame)]
-    for i in range(len(frames)):
-        previous_keyed_frame_i = i//frames_per_keyed_frame
-        next_keyed_frame_i = (i//frames_per_keyed_frame) + 1
-        fraction_between_keyframes = (i % frames_per_keyed_frame) / frames_per_keyed_frame  # Used for interpolation
-
-        # Get frames
-        previous_keyed_frame = keyed_frames[previous_keyed_frame_i]
-        next_keyed_frame = keyed_frames[next_keyed_frame_i] if next_keyed_frame_i < len(keyed_frames) else previous_keyed_frame
-        this_frame = frames[i]
-
-        # Interpolate between keyed frames.
-        for k, keyframe in previous_keyed_frame.keyframes.items():
-            if k not in next_keyed_frame.keyframes:  # User stopped being tracked. This should happen off-screen
-                continue
-            next_keyframe = next_keyed_frame.keyframes[k]
-
-            name = keyframe.name
-            count = lerp(keyframe.count, next_keyframe.count, fraction_between_keyframes)
-
-            this_keyframe = Keyframe(name, count)
-            this_keyframe.y_position = keyframe.y_position #slerp(keyframe.y_position, next_keyframe.y_position, fraction_between_keyframes)
-            this_keyframe.color = keyframe.color
-            this_frame.keyframes[name] = this_keyframe
+    return dataframe
 
 
+def get_colors(color_map, count):
+    """Gets a list of colours to be used in matplotlib
+    # See for colour mappings https://matplotlib.org/stable/gallery/color/colormap_reference.html
+    """
+    cols = plt.get_cmap(color_map, count + 1)  # +1 to prevent wrapping, where col 0 is same as col -1
+    cols = cols(range(count + 1)).tolist()  # Create a list of colours
+    return cols[:-1]  # Remove overlapping colour and return
 
-    # ==== Create the animation ==== #
+
+def main(path, frames_per_keyed_frame, users_to_display):
+    """
+    data_file_path - Path to the .csv file containing
+    """
+    dataframe = pandas.read_csv(path, index_col="date")
+    dataframe = prepare_dataframe(dataframe, frames_per_keyed_frame)
+    user_cols = dataframe.columns.values.tolist()[1:]  # Removes date column
+    users = [User(name=x, default_pos=-users_to_display) for x in user_cols]  # A list of all Users being tracked.
+    # Give users colours
+    cols = get_colors("twilight_shifted", len(users))
+    for user, col in zip(users, cols):
+        user.color = col
+
+    dates = []
+    for index, row in dataframe.iterrows():
+        dates.append(row[0])
+        row = row[1:]  # Removes date column
+        rankings = pandas.Series(row, dtype="int64").rank(method="first")
+        for user, count, ranking in zip(users, row, rankings):
+            user.count.append(count)
+            user.update_y_pos(ranking)
+
     fig = plt.Figure(figsize=(7, 6), dpi=144)
     ax = fig.add_subplot()
-    animator = MatAnimation(ax, dp_display_number, frames)
-    #a = animator.animate_frame(270)[0]
+    animator = MatAnimation(ax, users_to_display, users, dates)
+    #a = animator.animate_frame(60)[0]
     #fig.savefig("test.png")
     #return
 
-    anim = FuncAnimation(fig=fig, func=animator.animate_frame, frames=60*5, interval=(1/60)*1000, repeat=False)
-    anim.save("test_animation.mp4")
+    frame_num = dataframe.shape[0]
+    frame_duration = (1 / 30) * 1000  # How long each frame lasts.
+    anim = FuncAnimation(fig=fig, func=animator.animate_frame, frames=frame_num, interval=frame_duration, repeat=False)
+    anim.save("test.mp4")
 
 
 if __name__ == "__main__":
-    main()
+    # The path to the data recorded from the Discord bot.
+    data_file_path = "top_members.csv"
+    # How many frames each keyframes takes up. Note this is NOT framerate, which is always at 30 fps.
+    # This is just how long each line is displayed for. E.G 5 lines at 60 will be a 10 second animation.
+    frames_per_keyed_frame = 15
+
+    users_to_display = 10
+    main(data_file_path, frames_per_keyed_frame, users_to_display)
--- gather_member_keyframes.py
+++ gather_member_keyframes.py
@@ -16,17 +16,14 @@ class Admin(commands.Cog, name="admin"):
         if not await self.bot.has_perm(ctx, admin=True, message_on_fail=False): return
         # ==== Customization variables ==== #
         top_users = 10  # How many users will be displayed in the animation.
-        capture_interval = 86400  # How regularly, in seconds, to capture the state of the counter.
-        keyframes_path = "top_members.csv"  # Where to save the data to.
+        capture_interval = 24*60*60  # How regularly, in seconds, to capture the state of the counter.
+        output_path = "top_members.csv"  # Where to save the data to.
         date_format = "%Y-%m-%d"  # Currently set to year-month-day. See datetime.strftime for formatting options.
 
         # This system of copying the counters is a bit memory inefficient.
         # There might be more efficient ways to do this, but until it's an issue it's fine.
-        keyed_frames = []  # Stores the counter at each recorded moment.
-        top_users_buffer = top_users + 5  # How many users we'll record each capture. Used for animation reasons.
-        with open(keyframes_path, "w") as f:
-            f.writelines(f"{top_users},{top_users_buffer}\n")  # Metadata
-
+        keyed_frames = []  # Stores the counter and date at each keyed frame.
+        tracked_users = set()  # A non-repeating list of users who broke into the top_users
         message_count = 0  # Total messages done. Used to give updates on progress.
         c = Counter()  # This counts how many messages each user has sent so far.
 
@@ -35,43 +32,52 @@ class Admin(commands.Cog, name="admin"):
         first_message = first_message[0]
         c[first_message.author.name] += 1  # Add this message to the counter.
         start_snowflake = first_message.id
+        # ==== Tally messages ====
         while True:  # For me, this takes around 7 seconds each loop to search 1000 messages
             end_snowflake = add_to_snowflake(start_snowflake, capture_interval)  # One day later...
 
             # We've caught up to the current day!
-            if discord.utils.snowflake_time(end_snowflake) >= datetime.datetime.now():
+            if discord.utils.snowflake_time(start_snowflake) >= datetime.datetime.now():
                 break
 
             # Count each message for each person on this day...
             async for message in ctx.channel.history(limit=None, after=discord.Object(start_snowflake), before=discord.Object(end_snowflake), oldest_first=True):
                 c[message.author.name] += 1
+                message_count += 1
 
-            # Create a row for our data sheet...
-            current_tops = c.most_common(top_users_buffer)
-            current_date = discord.utils.snowflake_time(start_snowflake).strftime(date_format)
-            users = []
+            # Record which users are in the top_users
+            current_tops = c.most_common(top_users)
             for user in current_tops:
-                user_dict = {
-                    "ident": user[0],
-                    "count": user[1],
-                    "label": "",
-                    "col": ""
-                }
-                users.append(user_dict)
+                tracked_users.add(user[0])
 
-            # Add the keyframe to the file...
-            keyframe = [current_date, users]
-            with open(keyframes_path, "a") as f:
-                f.write(f"{json.dumps(keyframe)}\n")
+            # Record the counter and date
+            current_date = discord.utils.snowflake_time(start_snowflake).strftime(date_format)
+            keyed_frames.append((current_date, c.copy()))
 
+            # Loop handling
             start_snowflake = end_snowflake
-            message_count += 1
-            if message_count >= 60:
+            if len(keyed_frames) >= 60:
                 break
-        print(c)
 
+        # ==== Create the .csv file ====
+        tracked_users = sorted(list(tracked_users))
+        with open(output_path, "w", encoding="utf-8") as f:
+            name_list = ",".join(tracked_users)
+            f.write(f"date,{name_list}\n")
+            for keyed_frame in keyed_frames:
+                counter = keyed_frame[1]
+                user_data = []
+                for user in tracked_users:
+                    if user in counter:
+                        user_data.append(counter[user])
+                    else:
+                        user_data.append(0)
+                user_data_text = ",".join(str(x) for x in user_data)
+                f.write(f"{keyed_frame[0]},{user_data_text}\n")
 
         #await ctx.send("Got")
+        print("done")
+
 
 def add_to_snowflake(snowflake, seconds):
     return snowflake + (seconds * 1000 << 22)
